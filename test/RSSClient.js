2654a2641bcd53b8bb059c213644dcb70ee3c5ed
Emil Kjelsrud <emil@innit.no>
2014-03-06 10:45:35 +0100

    Added more tests

--- test/RSSClient.js
+++ test/RSSClient.js
@@ -2,11 +2,11 @@ var da = require('../deviantart.js');
 
 describe('RSSClient', function(){
 
+  var client = new da.RSSClient();
 
   describe('#submissions()', function(){
     
     it('should retrieve array without error', function(done){
-	  var client = new da.RSSClient();
       client.submissions("emilkje", function(err, data){
         if (err) throw err;
         if(data instanceof Array === false)
@@ -16,7 +16,6 @@ describe('RSSClient', function(){
     });
 
     it('should retrieve only images if option is specified', function(done){
-    	var client = new da.RSSClient();
     	client.submissions('emilkje', {type: 'image'}, function(err, data){
     		
     		if(err) throw err;
@@ -33,7 +32,6 @@ describe('RSSClient', function(){
     });
 
     it('should retrieve only notes if option is specified', function(done){
-    	var client = new da.RSSClient();
         this.timeout(3000);
 
     	client.submissions('MoonheartThunderClan', {type: 'note'}, function(err, data){
@@ -51,7 +49,6 @@ describe('RSSClient', function(){
 
 	
 	it('should return empty array if results are not found', function(done) {
-		var client = new da.RSSClient();
 		client.submissions('emilkje', {type: 'note'}, function(err, data){
 			if(err) throw err;
 			if(data.length > 0)
@@ -61,7 +58,23 @@ describe('RSSClient', function(){
 		});
 	});
 
-    /** TODO: Add tests for item attributes **/
-    
+	it('should return items with a set of defined attributes', function(done) {
+		client.submissions('emilkje', function(err, data){
+			if(err) throw err;
+			data.forEach(function(item){
+				if(!item.hasOwnProperty('title')) throw new Error('Item should have a title');
+				if(!item.hasOwnProperty('perma')) throw new Error('Item should have a permalink');
+				if(!item.hasOwnProperty('credit')) throw new Error('Item should have a credit object');
+				if(!item.hasOwnProperty('pubdate')) throw new Error('Item should have a publication date');
+				if(!item.hasOwnProperty('keywords')) throw new Error('Item should have a keywords array');
+				if(!item.hasOwnProperty('rating')) throw new Error('Item should have a rating');
+				if(!item.hasOwnProperty('category')) throw new Error('Item should have a category');
+				if(!item.hasOwnProperty('copyright')) throw new Error('Item should have a copyright');
+				if(!item.hasOwnProperty('description')) throw new Error('Item should have a description');
+			});
+			done();
+		});
+	});
+
   })
 })
