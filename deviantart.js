58e63df14487440d56b90c77b78da2d16247abab
Emil Kjelsrud <emil@innit.no>
2015-02-23 18:20:47 +0100

    Update env check

--- deviantart.js
+++ deviantart.js
@@ -1,6 +1,6 @@
 var RSSClient = require('./client/RSSClient');
 
-if(process.env.NODE_ENV !== "production")
+if(process.env.NODE_ENV == "dev")
 	require('longjohn');
 
 module.exports.RSSClient = RSSClient;
