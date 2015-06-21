3ec09bd7642a5db0379dddf4e8c1dbc0fdf556b4
Emil Kjelsrud <emil@innit.no>
2015-06-22 01:05:24 +0200

    Improved example to save all images

--- examples/file.stream.js
+++ examples/file.stream.js
@@ -1,12 +1,25 @@
-var da = require('../'),
-		client = new da.RSSClient("emilkje"),
-		fs = require('fs');
+var user = process.argv[2] || 'emilkje',
+da = require('../'),
+client = new da.RSSClient(user),
+fs = require('fs');
+
+var mkdirSync = function (path) {
+	try {
+		fs.mkdirSync(path);
+	} catch(e) {
+		if ( e.code != 'EEXIST' ) throw e;
+	}
+}
 
 client.images(function(err, images){
 	if(err) {
 		console.dir(err);
 	}
 
-	images[0].stream().pipe(fs.createWriteStream('image.jpg'));
-	console.log("saved image.jpg");
+	images.forEach(function(image){
+		mkdirSync('images');
+		image.stream().pipe(fs.createWriteStream(__dirname+'/images/'+image.title+"_by_"+image.copyright.text+'.jpg'));
+	});
+
+	console.log('saved ' + user + 's photos in images/');
 });
